63f1af4ec3923ba35292e74b098c79fd39213f32
Александр Востриков <aptimict11@gmail.com>
2023-05-13 20:47:42 +0300

    Реорганизация структуры проекта. Создание RouterProtocol и класса Router

---
+++ Navigation/Library/Router/Protocol/RouterProtocol.swift
@@ -0,0 +1,32 @@
+//
+//
+// RouterProtocol.swift
+// Navigation
+//
+// Created by Александр Востриков
+//
+    
+
+import Foundation
+
+protocol RouterProtocol: Presentable {
+    func present(_ module: Presentable?)
+    func present(_ module: Presentable?, animated: Bool)
+    
+    func push(_ module: Presentable?)
+    func push(_ module: Presentable?, hideBottomBar: Bool)
+    func push(_ module: Presentable?, animated: Bool)
+    func push(_ module: Presentable?, animated: Bool, completion: (() -> Void)?)
+    func push(_ module: Presentable?, animated: Bool, hideBottomBar: Bool, completion: (() -> Void)?)
+    
+    func popModule()
+    func popModule(animated: Bool)
+    
+    func dismissModule()
+    func dismissModule(animated: Bool, completion: (() -> Void)?)
+    
+    func setRootModule(_ module: Presentable?)
+    func setRootModule(_ module: Presentable?, hideBar: Bool)
+    
+    func popToRootModule(animated: Bool)
+  }
